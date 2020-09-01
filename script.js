1df6d7eaf9b41ec03692a94bd2c6d2d38ae31ae8
James Howard <jth2698@gmail.com>
2020-08-31 21:32:45 -0500

    js

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 const searchInput = $("#search-input");
 const searchBtn = $("#search-button");
 
-searchBtn.on("click", function() {
+searchBtn.on("click", function () {
 
-    const query = searchInput.val();
+    const baseURL = "https://api.bestbuy.com/v1/";
+
+    let searchType = "products";
+
+    let query = searchInput.val();
+    query = query.toString();
+    query = query.trim();
 
     console.log(query);
-    
-    var BBApiKey = "GGmupVaRMy1eDvoIlNss1A0G";
 
-    var BBUrl = "https://api.bestbuy.com/v1/products?format=json&show=all&apiKey=" + BBApiKey;
+    let queryURL = "((search=" + query + ")";
+
+    let activeProducts = "&sku=*";
+
+    let inStoreURL = "&inStoreAvailability=true)?";
+
+    let pageSize = "pageSize=100"
+
+    let cursorMark = "&cursorMark=*"
+
+    let JSONFormat = "&format=json";
+
+    let BBAPIKey = "&apiKey=GGmupVaRMy1eDvoIlNss1A0G";
+
+    productURL = baseURL + searchType + queryURL + activeProducts + inStoreURL + pageSize + cursorMark + BBAPIKey + JSONFormat;
+
+    console.log(productURL);
 
     $.ajax({
-        url: BBUrl,
+        url: productURL,
         method: "GET",
 
-    }).then(function(response) {
+    }).then(function (response) {
 
-        console.log(response);
+        console.log(productURL);
 
-        const resultsLeft = $("#results-left");
+        let allProducts = response.products;
 
-        const productArray = response.products;
+        let allProductSKUs = [];
 
-        for (i = 0; i < productArray.length; i++) {
-            productName = response.products[i].name;
-            if (productName.includes(query)) {
-                const resultDiv = $("<div></div>");
-                resultDiv.text(productName + "-" + productName.regularPrice);
-                resultsLeft.append(resultDiv);
-            }
+        for (i = 0; i < allProducts.length; i++) {
+
+            let productSKU = allProducts[i].sku;
+            allProductSKUs.push(productSKU);
         }
+
+        console.log(allProductSKUs);
     })
+
 })
