b90baa37a64b2a4d17190b443f0bc782f885062a
James Howard <jth2698@gmail.com>
2020-09-02 18:54:09 -0500

    ajax for storeURL

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ const searchBtn = $("#search-button");
 
 searchBtn.on("click", function () {
 
-    const baseURL = "https://api.bestbuy.com/v1/";
-
-    let searchType = "products";
+    const baseURL = "https://api.bestbuy.com/v1/products";
 
     let query = searchInput.val();
     query = query.toString();
     query = query.trim();
 
-    console.log(query);
+    let userPostalCode = "";
 
     let queryURL = "((search=" + query + ")";
 
@@ -23,13 +21,11 @@ searchBtn.on("click", function () {
 
     let cursorMark = "&cursorMark=*"
 
-    let JSONFormat = "&format=json";
+    let format = "&format=json";
 
     let BBAPIKey = "&apiKey=GGmupVaRMy1eDvoIlNss1A0G";
 
-    productURL = baseURL + searchType + queryURL + activeProducts + inStoreURL + pageSize + cursorMark + BBAPIKey + JSONFormat;
-
-    console.log(productURL);
+    let productURL = baseURL + searchType + queryURL + activeProducts + inStoreURL + pageSize + cursorMark + BBAPIKey + format;
 
     $.ajax({
         url: productURL,
@@ -50,6 +46,30 @@ searchBtn.on("click", function () {
         }
 
         console.log(allProductSKUs);
-    })
 
-})
+        for (i = 0; i < allProductSKUs.length; i++) {
+
+            let skuInsert = "/" + allProductSKUs[i] + "/stores.json?";
+
+            let postalCodeInsert = "postalCode=" + postalCode;
+
+            let storeURL = baseURL + skuInsert + postalCodeInsert + BBAPIKey;
+
+            $.ajax({
+                url: storeURL,
+                method: "GET",
+
+            }).then(function (response) {
+
+                console.log(storeURL);
+
+
+
+                let localavailabilityURL = queryURL +
+
+                    https://api.bestbuy.com/v1/stores(area(78751,100))+products(sku%20in())?show=storeId,name,products.sku,products.name&format=json&apiKey=GGmupVaRMy1eDvoIlNss1A0G
+
+                    url_store = 'https://api.bestbuy.com/v1/products/' + document.getElementById('partnumber').value.trim() + '/stores.json?postalCode=' + document.getElementById('zipcode').value.trim() + '&apiKey=08JJS1ffSirGzNn7hMjRcjBN' + '&irclickid=yXE2NrTAhxyJU580MdV3iVCmUklQd9yBr2QUUA0&irgwc=1&ref=198&loc=126338&acampID=614286';
+            })
+
+        })
